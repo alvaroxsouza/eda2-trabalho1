5416c9ae688ca8d6f439f7effbc16648a16526ca
NessaMd <vanessa.machado@ufba.br>
2023-04-27 15:30:32 -0300

    correção em perfectHash

--- perfectHash.cpp
+++ perfectHash.cpp
@@ -27,20 +27,19 @@ void gerar_variaveis_aleatorias(int &a, int &b) {
     b = rand() % 101;
 }
 
-// Função de inserção 
-void insere(vector<vector<int>>& nivel_um, vector<vector<int>>& nivel_dois, int quantidade_de_registros, int a, int b, int& total_acessos,int& total_chaves_inseridas) {
+void insere(vector<vector<int>>& nivel_um, vector<vector<int>>& nivel_dois, int quantidade_de_registros, int a, int b, int& total_acessos, int& total_chaves_inseridas) {
     vector<int> chaves; // Um vetor de chaves, auxiliar, é usado para armazenar as chaves, passadas na entrada, que serão inseridas nos registros
     int chave;
     cin >> chave;
 
-    while (chave != 0) { // armazenando as chaves fornecidas na entrada em um vetor de chaves (auxiliar)
+    while (chave != 0) { // armazendo as chaves fornecidas na entrada em um vetor auxiliar
         chaves.push_back(chave);
         cin >> chave;
     }
-    
-    // a função de hash é dada por: h = ((a * chave + b) mod 101) mod quantidade de registros. 101 é o maior primo, definido por convenção
 
-    // Caso não haja choque de chaves - inserção na tabela principal
+    // a função de hash é dada por: h = ((a * chave + b) mod 101) mod quantidade de registros. 101 é o maior primo, definido previamente.
+
+    // Inserção na tabela principal
     for (int i = 0; i < chaves.size(); i++) {
         int h = ((a * chaves[i] + b) % 101) % quantidade_de_registros; // cálculo do hash
         nivel_um[h].resize(nivel_um[h].size() + 1, chaves[i]); // insere a chave no nível um
@@ -48,35 +47,39 @@ void insere(vector<vector<int>>& nivel_um, vector<vector<int>>& nivel_dois, int 
         total_chaves_inseridas++; // incrementa o total de chaves inseridas
     }
 
-    // Caso  haja choque de chaves - inserção nas tabelas secundárias
+    /*Inserção nas tabelas secundárias - quando em uma dada posição na tabela principal há apenas uma chave , essa posição 
+    na tabela secundária tem tamanho igual a um referente a única chave a ser inserida. Por outro lado, quando em uma dada
+    posição na tabela principal há mais de uma chave, essa posição na tabela secundária tem tamanho igual ao número de chaves
+    nessa posição elevado ao quadrado*/
     for (int i = 0; i < quantidade_de_registros; i++) {
         int n = nivel_um[i].size();
-        if (n > 1) {
-            nivel_dois[i].resize(n*n, -1); // nível dois de cada posição tem o tamanho do número de chaves que chocaram no nivel um, elevedo ao quadrado
+        if (n == 1) {
+            int h = ((a * nivel_um[i][0] + b) % 101) % quantidade_de_registros;
+            nivel_dois[h].resize(1, nivel_um[i][0]);
+            total_acessos++; // incrementa o total de acessos
+        } else if (n > 1) {
+            nivel_dois[i].resize(n * n);
             for (int j = 0; j < n; j++) {
-                int h = ((a * nivel_um[i][j] + b) % 101) % (n*n); // cálculo do hash
-                if (nivel_dois[i][h] == -1) {
+                int h = ((a * nivel_um[i][j] + b) % 101) % (n * n);
+                if (nivel_dois[i][h] == 0) {
                     nivel_dois[i][h] = nivel_um[i][j];
+                    total_acessos++; // incrementa o total de acessos
                 } 
                 else {
                     int k = h; // variável k, variável auxiliar que recebe o valor de hash
                     do {
-                        k = (k + 1) % (n*n); // atualiza o valor de hash até que aja posição buscada disponível
+                        k = (k + 1) % (n * n); // atualiza o valor de hash até que aja posição buscada disponível
                         total_acessos++; // incrementa o total de acessos
-                    } while (nivel_dois[i][k] != -1); // enquanto houver posições disponíveis
+                    } while (nivel_dois[i][k] != 0); 
                     nivel_dois[i][k] = nivel_um[i][j]; /*insere a chave no nível dois (não incrementa o total de chaves inseridas pois já foi incrementada
                                                          na primeira passada, no nível um)*/
                 }
-                total_acessos++; // incrementa o total de acessos
             }
         }
     }
 }
 
-
-/*Função para cálculo da média de acessos realizados para a inserção de um dado bloco de chaves
-A média de acessos é dada pela divisão entre o total de acessos realizados, para a inserção de todas as chaves, e o 
-número de chaves inseridas*/
+// Função para cálculo da média de acessos realizados para a inserção de um dado bloco de chaves
 double calcula_media_acessos(int total_acessos, int total_chaves_inseridas) {
     if(total_chaves_inseridas == 0) return 0;
     return (double) total_acessos / total_chaves_inseridas;
@@ -100,14 +103,13 @@ void saida(vector<vector<int>>& nivel_um, vector<vector<int>>& nivel_dois, int q
     }
     cout << endl;
 
-    for (int i = 0; i < quantidade_de_registros; i++) {
-        
-        if (nivel_um[i].size() > 1) {
+    for (int i = 0; i < quantidade_de_registros; i++) {   
+        if (nivel_um[i].size() != 0) {
             cout << "Nivel Dois - secundario:" << endl;
             cout << "posicao " << i << ":" << endl;
             for (int j = 0; j < nivel_dois[i].size(); j++) {
-                if (nivel_dois[i].size() > 1){
-                    if (nivel_dois[i][j] == -1) {
+                if (nivel_dois[i].size() != 0){
+                    if (nivel_dois[i][j] == 0) {
                         cout << j << ": registro vazio" << endl;
                     } 
                     else {
@@ -115,6 +117,7 @@ void saida(vector<vector<int>>& nivel_um, vector<vector<int>>& nivel_dois, int q
                     }
                 }                
             }
+            cout << endl;
         }
 
     }
@@ -126,7 +129,7 @@ void saida(vector<vector<int>>& nivel_um, vector<vector<int>>& nivel_dois, int q
 
 /* Entrada:
 1ª linha: número de registros
-2ª linha: valor das chaves digitas em sequência separadas por um espaço
+Linhas seguintes: valor das chaves a serem inseridas
 
 - A inserção para ao digitar o valor 0 (zero)
 */
