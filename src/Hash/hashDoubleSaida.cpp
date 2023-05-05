bc49289ad006a1c97228d377b3d7868c5b697a3b
Alvaro <alvarosouza61@gmail.com>
2023-05-05 16:15:31 -0300

    Ajustes na saída do programa e organização

--- src/Hash/hashDoubleSaida.cpp
+++ src/Hash/hashDoubleSaida.cpp
@@ -9,17 +9,6 @@ using namespace std;
 using namespace HashDoubleClass;
 
 void HashDouble::saidaDoubleHash(vector<long int>& tabela_hash, long int quantidade_de_registros) {
-    // cout << "################### Hashing Duplo ####################" << endl;
-    // cout << "################ Tabela de registros #################" << endl;
-    // for (long int i = 0; i < quantidade_de_registros; i++) {
-    //     if (tabela_hash[i] == -1) {
-    //         cout << "Registro " << i << ": Registro Vazio" << endl;
-    //     }
-    //     else {
-    //         cout << "Registro " << i << ": " << tabela_hash[i] << endl;
-    //     }
-    // }
-    // cout << "######################################################" << endl;
     cout << "######################################################" << endl;
     cout << "################### Hashing Duplo ####################" << endl;
     cout << "######################################################" << endl;
--- src/Hash/hashPerfectSaida.cpp
+++ src/Hash/hashPerfectSaida.cpp
@@ -67,44 +67,6 @@ void HashPerfect::saidaPefectHash(
     vector<vector<long int>>& nivel_um,
     vector<vector<long int>>& nivel_dois,
     long int quantidade_de_registros) {
-    /* cout << "################## Hashing Perfeito ###################" << endl;
-    cout << "################ Tabela de registros ##################" << endl;
-    cout << "Nivel um - principal:" << endl;
-    for (long int i = 0; i < quantidade_de_registros; i++) {
-        cout << "registro " << i << ": ";
-        if (nivel_um[i].empty()) {
-            cout << "registro vazio" << endl;
-        } else {
-            // cout << "Tamanho do registro: " << i << nivel_um[i].size() << endl;
-            for (unsigned long int j = 0; j < nivel_um[i].size(); j++) {
-                // cout << "Entrou aqui" << endl;
-                cout << nivel_um[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
-    cout << endl;
-
-    for (long int i = 0; i < quantidade_de_registros; i++) {   
-        if (nivel_um[i].size() != 0) {
-            cout << "Nivel Dois - secundario:" << endl;
-            cout << "registro " << i << ":" << endl;
-            for (unsigned long int j = 0; j < nivel_dois[i].size(); j++) {
-                if (nivel_dois[i].size() != 0){
-                    if (nivel_dois[i][j] == 0) {
-                        cout << j << ": registro vazio" << endl;
-                    } 
-                    else {
-                        cout << j << ": " << nivel_dois[i][j] << endl;
-                    }
-                }                
-            }
-            cout << endl;
-        }
-
-    }
-    cout << "#######################################################" << endl; */
-
     cout << "###########################################################" << endl;
     cout << "#################### Hashing Perfeito #####################" << endl;
     cout << "###########################################################" << endl;
